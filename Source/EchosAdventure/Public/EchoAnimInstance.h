235413581e4710c5736e12fa5db68b9d96069c58
NanKinXH <finn108039@gmail.com>
2023-02-04 23:41:27 +0800

    update: add animation class

---
+++ Source/EchosAdventure/Public/EchoAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "EchoAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ECHOSADVENTURE_API UEchoAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
