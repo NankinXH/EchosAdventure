9a0adbe2afebdf0f9fa168166f6ec0e923cc75e8
NanKinXH <finn108039@gmail.com>
2023-02-05 03:40:20 +0800

    push

--- Source/EchosAdventure/Private/EchoAnimInstance.cpp
+++ Source/EchosAdventure/Private/EchoAnimInstance.cpp
@@ -1,5 +1,26 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "EchoAnimInstance.h"
+#include "Echo.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
+void UEchoAnimInstance::NativeInitializeAnimation()
+{
+    Super::NativeInitializeAnimation();
+
+    EchoCharacter = Cast<AEcho>(TryGetPawnOwner());
+    if (EchoCharacter)
+    {
+        EchoCharacterMovement = EchoCharacter->GetCharacterMovement();
+    }
+}
+
+void UEchoAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+    Super::NativeUpdateAnimation(DeltaTime);
+    if (EchoCharacterMovement)
+    {
+        GroundSpeed = UKismetMathLibrary::VSizeXY(SlashCharacterMovement->Velocity)
+    }
+}
--- Source/EchosAdventure/Public/EchoAnimInstance.h
+++ Source/EchosAdventure/Public/EchoAnimInstance.h
@@ -7,11 +7,21 @@
 #include "EchoAnimInstance.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class ECHOSADVENTURE_API UEchoAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation() override;
+
+	UPROPERTY(BlueprintReadOnly)
+	class AEcho *EchoCharacter;
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	class UCharacterMovementComponent *EchoCharacterMovement;
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	float GroundSpeed
 };
